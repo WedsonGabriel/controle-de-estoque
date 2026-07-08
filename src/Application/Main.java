c358dc928de913786072c13383fcefaa2c90a277
ALVlN <jhonata_fsantos@hotmail.com>
2026-07-07 22:23:40 -0300

    Adicionando a base das classes

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,11 +1,28 @@
 package Application;
 import java.util.Locale;
-import java.util.Scanner;
+import Entities.Categoria;
+import Entities.Estoque;
+import Entities.Produto;
 
 public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
+
+        Categoria jogos = new Categoria("Jogos");
+
+        Produto GTA6 = new Produto("Grand Theft Auto 6", 550.00, jogos);
+        Produto TheSims4 = new Produto("The Sims 4", 6000.00, jogos);
+
+        Estoque estoque = new Estoque();
+
+        estoque.adicionarProduto(GTA6, 6);
+        estoque.adicionarProduto(TheSims4, 4);
+        estoque.adicionarProduto(GTA6, 100);
+
+        estoque.listarEstoque();
+
+        estoque.removerProduto(GTA6, 6);
+        estoque.listarEstoque();
 
     }
 }
--- src/Entities/Produto.java
+++ src/Entities/Produto.java
@@ -1,5 +1,39 @@
 package Entities;
+import java.util.Objects;
 
 public class Produto {
+    private String nome;
+    private double preco;
+    private Categoria categoria;
 
+    public Produto(String nome, double preco, Categoria categoria){
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public double getPreco(){
+        return preco;
+    }
+
+    public Categoria getCategoria(){
+        return categoria;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return Objects.equals(nome, produto.nome);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(nome);
+    }
 }
